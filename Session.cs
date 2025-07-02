303b43244acab32f1ea77a528505fd96001086b1
Beatriceoira <89373884+Beatriceoira@users.noreply.github.com>
2025-07-03 03:07:09 +0800

    Add files via upload

---
+++ Session.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_SPORTS
+{
+    public static class Session
+    {
+        public static int UserID;
+        public static string Username;
+        public static string RoleName;
+        public static string Permission;
+       
+    }
+
+}
